b3d033716603290443cca83c4ff8d70ea229f3df
amrilsyaifa <amrilsyaifa@gmail.com>
2018-10-11 07:45:06 +0700

    GET and DELETE

--- api/routes/products.js
+++ api/routes/products.js
@@ -27,4 +27,16 @@ router.get('/:productId', (req, res, next) => {
     }
 })
 
+router.patch('/:productId', (req, res, next) => {
+    res.status(200).json({
+        message: "Patch /products/:productID",
+    })
+})
+
+router.delete('/:productId', (req, res, next) => {
+    res.status(200).json({
+        message: "Patch /products/:productID",
+    })
+})
+
 module.exports = router
