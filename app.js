a210925adedd044561e7a21f50adc483a7c19d04
amrilsyaifa <amrilsyaifa@gmail.com>
2018-10-13 13:50:31 +0700

    bodyParser

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
+const bodyParser = require('body-parser')
 
 app.use(morgan('dev'))
+app.use(bodyParser.urlencoded({ extended : false}))
+app.use(bodyParser.json())
 
 const productRouter = require('./api/routes/products')
 const orderRouter = require('./api/routes/orders')
